4b202c5d61f350cb244426ba0ff5ce1e34910971
kettaleb <k.ettaleb@gmail.com>
2022-04-11 17:05:02 -0500

    Added application properties file

--- src/main/java/com/revature/models/Reimbursement.java
+++ src/main/java/com/revature/models/Reimbursement.java
@@ -1,5 +1,7 @@
 package com.revature.models;
 
+import java.time.LocalDate;
+
 /**
  * This concrete Reimbursement class can include additional fields that can be used for
  * extended functionality of the ERS application.
@@ -14,6 +16,10 @@ package com.revature.models;
  *
  */
 public class Reimbursement extends AbstractReimbursement {
+	
+	private String description;
+	private LocalDate creationDate;
+	private LocalDate resolutionDate;
 
     public Reimbursement() {
         super();
@@ -23,7 +29,38 @@ public class Reimbursement extends AbstractReimbursement {
      * This includes the minimum parameters needed for the {@link com.revature.models.AbstractReimbursement} class.
      * If other fields are needed, please create additional constructors.
      */
-    public Reimbursement(int id, Status status, User author, User resolver, double amount) {
+    public Reimbursement(int id, Status status, User author, User resolver, double amount, 
+    	   String description,LocalDate creationDate, LocalDate resolutionDate) {
+    	
         super(id, status, author, resolver, amount);
+        this.description = description;
+        this.creationDate = creationDate;
+        this.resolutionDate = resolutionDate;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public LocalDate getResolutionDate() {
+		return resolutionDate;
+	}
+
+	public void setResolutionDate(LocalDate resolutionDate) {
+		this.resolutionDate = resolutionDate;
+	}
+    
+    
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -15,7 +15,12 @@ package com.revature.models;
  *
  */
 public class User extends AbstractUser {
-
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	
     public User() {
         super();
     }
@@ -26,5 +31,73 @@ public class User extends AbstractUser {
      */
     public User(int id, String username, String password, Role role) {
         super(id, username, password, role);
+        
     }
+    
+    public User(int id, String firstName, String lastName, String username, String password, String email, String address, Role role) {
+        super(id, username, password, role);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        
+    }
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+    
+    
 }
